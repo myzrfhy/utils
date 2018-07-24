4225586535088c56412b1796bc27ec0299728019
留一晗 <liuyihan@u51.com>
2018-07-24 17:56:34 +0800

    Test: Varmanager test compile fix

--- variablemanager/src/test/java/com/enniu/cloud/services/fc/loan/platform/utils/thread/MultiSharingTest.java
+++ variablemanager/src/test/java/com/enniu/cloud/services/fc/loan/platform/utils/thread/MultiSharingTest.java
@@ -6,7 +6,7 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import com.enniu.cloud.services.fc.loan.platform.utils.varmanager.ThreadLocalManager;
+import com.enniu.cloud.services.fc.loan.platform.utils.varmanager.VarManager;
 import com.enniu.cloud.services.fc.loan.platform.utils.varmanager.thread.VarManagerThreadWrapper;
 import java.util.concurrent.CountDownLatch;
 import org.junit.Test;
@@ -26,14 +26,14 @@ public class MultiSharingTest extends MockBase{
     public void multiSharing_extend() {
         String key = "aaa";
         String value = "val";
-        ThreadLocalManager.open();
-        ThreadLocalManager.put(key, value);
+        VarManager.open();
+        VarManager.put(key, value);
 
         Print print = mock(Print.class);
         doNothing().when(print).print(eq(key));
 
         CountDownLatch latch = new CountDownLatch(1);
-        TestUtils.submit(VarManagerThreadWrapper.multiSharing(()->print.print(ThreadLocalManager.get(key))), latch);
+        TestUtils.submit(VarManagerThreadWrapper.multiSharing(()->print.print(VarManager.get(key))), latch);
 
         verify(print).print(eq(value));
     }
@@ -46,15 +46,15 @@ public class MultiSharingTest extends MockBase{
         String key = "aaa";
         String value = "val";
 
-        ThreadLocalManager.open();
+        VarManager.open();
 
         Print print = mock(Print.class);
         doNothing().when(print).print(eq(key));
 
         CountDownLatch latch = new CountDownLatch(1);
-        TestUtils.submit(VarManagerThreadWrapper.multiSharing(() -> ThreadLocalManager.put(key, value)), latch);
+        TestUtils.submit(VarManagerThreadWrapper.multiSharing(() -> VarManager.put(key, value)), latch);
 
-        print.print(ThreadLocalManager.get(key));
+        print.print(VarManager.get(key));
         verify(print).print(eq(value));
     }
 
@@ -65,16 +65,16 @@ public class MultiSharingTest extends MockBase{
     public void multiSharing_child_remove() {
         String key = "aaa";
         String value = "val";
-        ThreadLocalManager.open();
-        ThreadLocalManager.put(key, value);
+        VarManager.open();
+        VarManager.put(key, value);
 
         Print print = mock(Print.class);
         doNothing().when(print).print(eq(key));
 
         CountDownLatch latch = new CountDownLatch(1);
-        TestUtils.submit(VarManagerThreadWrapper.multiSharing(()->ThreadLocalManager.remove(key)), latch);
+        TestUtils.submit(VarManagerThreadWrapper.multiSharing(()->VarManager.remove(key)), latch);
 
-        print.print(ThreadLocalManager.get(key));
+        print.print(VarManager.get(key));
         verify(print).print(eq(null));
     }
 
